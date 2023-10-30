c8e2f163690c72e28312bcaf515d62c8e2656cc9
yessine <yessinekarmous25@gmail.com>
2023-10-30 17:40:16 +0100

    Fundemantels 1

---
+++ Week 1/Day 1/Practice/Fundemantels1/Program.cs
@@ -0,0 +1,38 @@
+﻿for(int i=1;i<255; i++)
+{
+    Console.WriteLine(i);
+}
+
+for(int i=1;i<100;i++)
+{
+ if (i % 3 == 0 || i % 5 == 0){
+    Console.WriteLine(i);
+ }
+
+}
+for(int i=1;i<100;i++)
+{
+ if (i % 3 == 0 ){
+    Console.WriteLine("FIZZ");
+ }
+ else if(i%5==0){
+    Console.WriteLine("BUZZ");
+ }
+ else if(i % 3 == 0 || i % 5 == 0){
+    Console.WriteLine("FIZZBUZZ");
+ }
+}
+// with While 
+int j =1;
+while (j<100){
+     if (j % 3 == 0 ){
+    Console.WriteLine("FIZZ");
+ }
+ else if(j%5==0){
+    Console.WriteLine("BUZZ");
+ }
+ else if(j % 3 == 0 || j % 5 == 0){
+    Console.WriteLine("FIZZBUZZ");
+ }
+ j++;
+}
