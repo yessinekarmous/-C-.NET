0d0fbb2da7f8f8a283e842f0c651392bd529db4b
yessine <yessinekarmous25@gmail.com>
2023-10-31 12:24:27 +0100

    Boxing/Unboxing

---
+++ Week 1/Day 2/Practice/Boxing/Program.cs
@@ -0,0 +1,19 @@
+﻿List <object> MyList=new List<object>();
+MyList.Add(7);
+MyList.Add(28);
+MyList.Add(-1);
+MyList.Add(true);
+MyList.Add("chair");
+
+foreach(object One in MyList){
+    Console.WriteLine(One);
+}
+int s=0;
+for(int i=0;i<MyList.Count;i++){
+    if (MyList[i] is int)
+    {
+        s= s+ (int)MyList[i];
+    }
+}
+Console.WriteLine("The Sum is: "+ s);
+Console.WriteLine($"The Sum is: {s}");
