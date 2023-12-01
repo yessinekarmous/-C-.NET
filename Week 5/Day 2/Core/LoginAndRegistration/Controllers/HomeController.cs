4e92d63a38d1a8861ca18bc004ee4042edd77d58
yessine <yessinekarmous25@gmail.com>
2023-12-01 16:42:31 +0100

    logand reg update

--- Week 5/Day 2/Core/LoginAndRegistration/Controllers/HomeController.cs
+++ Week 5/Day 2/Core/LoginAndRegistration/Controllers/HomeController.cs
@@ -2,66 +2,109 @@
 using Microsoft.AspNetCore.Mvc;
 using LoginAndRegistration.Models;
 using Microsoft.AspNetCore.Identity;
-
+// using Microsoft.AspNetCore.Http;
+// using System.Security.Claims; 
+// using System.Threading;
 namespace LoginAndRegistration.Controllers;
+// using System;
+// using System.Security.Principal;
+// using Microsoft.AspNetCore.Http;
+
+
 
 public class HomeController : Controller
-{   private MyContext _context;
+{
+    private MyContext _context;
     private readonly ILogger<HomeController> _logger;
+    // private readonly IHttpContextAccessor _httpContextAccessor;
 
     public HomeController(ILogger<HomeController> logger, MyContext context)
     {
-        _context=context;
+        _context = context;
         _logger = logger;
+        // _httpContextAccessor = httpContextAccessor;
     }
-
+    // private void SetPrincipal(IPrincipal principal)
+    // {
+    //     if (principal is ClaimsPrincipal claimsPrincipal)
+    //     {
+    //         Thread.CurrentPrincipal = claimsPrincipal;
+    //         if (_httpContextAccessor.HttpContext != null)
+    //         {
+    //             _httpContextAccessor.HttpContext.User = claimsPrincipal;
+    //         }
+    //     }
+    // }
     public IActionResult Index()
     {
         return View();
     }
     [HttpPost("")]
-    public IActionResult Form(User NewUser){
-        if(ModelState.IsValid){
+    public IActionResult Form(User NewUser)
+    {
+        if (ModelState.IsValid)
+        {
 
-            if(_context.Users.Any(a=>a.Email==NewUser.Email))
+            if (_context.Users.Any(a => a.Email == NewUser.Email))
             {
-                ModelState.AddModelError("Email" ,"Email already in use");
+                ModelState.AddModelError("Email", "Email already in use");
                 return View("Index");
+            }else{
+                PasswordHasher<User> hasher = new PasswordHasher<User>();
+                NewUser.Password = hasher.HashPassword(NewUser, NewUser.Password);
+                _context.Add(NewUser);
+                _context.SaveChanges();
+                HttpContext.Session.SetInt32("userId",NewUser.UserId);
+                return RedirectToAction("Success"); 
             }
-            _context.Add(NewUser);
-            PasswordHasher<User> hasher=new PasswordHasher<User>();
-            NewUser.Password= hasher.HashPassword(NewUser,NewUser.Password);
-            _context.SaveChanges();
-            return RedirectToAction("Success");               
-                }
-        else{
+            
+        }
+        else
+        {
             return View("Index");
         }
     }
     [HttpGet("Success")]
-    public IActionResult Success(){
-        return View();
+    public IActionResult Success()
+    {
+        if(HttpContext.Session.GetInt32("userId")==null){
+            return RedirectToAction("Index");
+        }else{
+            return View("Success");
+        }  
+    }
+    [HttpGet("logout")]
+    public IActionResult Logout(){
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
     }
     [HttpPost("/users/login")]
-    public IActionResult Login(LogUser NewLogUser){
-        if(ModelState.IsValid){
-            User UserfromDb=_context.Users.FirstOrDefault(a=>a.Email==NewLogUser.LogEmail);
-            if(UserfromDb==null){
-                ModelState.AddModelError("LogEmail","No User Found");
+    public IActionResult Login(LogUser NewLogUser)
+    {
+        if (ModelState.IsValid)
+        {
+            User UserfromDb = _context.Users.FirstOrDefault(a => a.Email == NewLogUser.LogEmail);
+            if (UserfromDb == null)
+            {
+                ModelState.AddModelError("LogEmail", "No User Found");
                 return View("Index");
             }
-            PasswordHasher<LogUser> hasher=new PasswordHasher<LogUser>();
-            var result= hasher.VerifyHashedPassword(NewLogUser,UserfromDb.Password,NewLogUser.LogPassword);
-            if(result==0){
-                ModelState.AddModelError("Email" ,"Email already in use");
+            PasswordHasher<LogUser> hasher = new PasswordHasher<LogUser>();
+            var result = hasher.VerifyHashedPassword(NewLogUser, UserfromDb.Password, NewLogUser.LogPassword);
+            if (result == 0)
+            {
+                ModelState.AddModelError("Email", "Email already in use");
                 return View("Index");
             }
-            else{
+            else
+            {
+                HttpContext.Session.SetInt32("userId",UserfromDb.UserId);
                 return RedirectToAction("Success");
             }
-            
+
         }
-        else{
+        else
+        {
             return View("Index");
         }
     }
--- Week 5/Day 2/Core/LoginAndRegistration/Program.cs
+++ Week 5/Day 2/Core/LoginAndRegistration/Program.cs
@@ -10,6 +10,10 @@ builder.Services.AddDbContext<MyContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
+builder.Services.AddHttpContextAccessor();  // AddHttpContextAccessor gives our views direct access to session so that session data doesn't need to be repeatedly passed into the ViewBag.
+builder.Services.AddSession();
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -20,6 +24,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+  // add this line before calling the builder.Build() method
+app.UseSession();    // add this line before calling the app.MapControllerRoute() method
+
+
 app.UseStaticFiles();
 
 app.UseRouting();
